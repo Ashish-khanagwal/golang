f07963d6f6149ed25d5790241bd44d18aa9cf038
Ashish Khanagwal <ashishkhanagwal2001@gmail.com>
2023-03-31 23:53:11 +0530

    Day-2 of GoLang done

--- Booking-App/main.go
+++ Booking-App/main.go
@@ -7,13 +7,46 @@ func main() {
 	// var conferenceName = "Go Conference"
 	conferenceName := "Go Conference"
 	const conferenceTickets = 50
-	var remaininTickets = 50
+	var remainingTickets uint = 50
+
+	// var bookings [50]string ### This is how we declare arrays but this is not the convinient way to do so.
+	bookings := []string{} // We use Slice instead
 
 	// fmt.Println("Welcome to our", conferenceName, "booking application")
 	// fmt.Println("We have total of", conferenceTickets, "tickets and", remaininTickets, "are still available")
 
 	fmt.Printf("Welcome to our %v booking application\n", conferenceName)
-	fmt.Printf("We have a total of %v tickets and %v tickets are still available\n", conferenceTickets, remaininTickets)
+	fmt.Printf("We have a total of %v tickets and %v tickets are still available\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
+	var (
+		userFirstName string
+		userLastName  string
+		userTickets   uint
+		userEmail     string
+		userMobile    uint
+	)
+
+	fmt.Println("Enter your first name:")
+	fmt.Scan(&userFirstName)
+
+	fmt.Println("Enter your last name:")
+	fmt.Scan(&userLastName)
+
+	fmt.Println("Enter your email-id:")
+	fmt.Scan(&userEmail)
+
+	fmt.Println("Enter your Mobile number:")
+	fmt.Scan(&userMobile)
+
+	fmt.Println("Number of tickets you want to book:")
+	fmt.Scan(&userTickets)
+
+	remainingTickets -= userTickets
+	// bookings[0] = userFirstName + " " + userLastName ### This is not the right way to do.
+	bookings = append(bookings, userFirstName+" "+userLastName) // "Append" adds the elements at the end of the slice
+
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will recieve an confirmation email at %v\n", userFirstName, userLastName, userTickets, userEmail)
+	fmt.Printf("%v tickets are remaining for the %v, Share to your friends\n", remainingTickets, conferenceName)
+	fmt.Printf("This is all our bookings: %v\n", bookings)
 }
--- hello.go
+++ hello.go
@@ -55,12 +55,35 @@ func main() {
 
 	// ########### POINTERS ###########
 
-	// Pointer is a variable that stores the memory address of another variable. And this address points to the actual value of the variable\
+	// Pointer is a variable that stores the memory address of another variable. And this address points to the actual value of the variable
 	// In Go, we can declare a pointer by using "*" symbol before the variable name.
 	// And, we can get the memory address of a variable by using "&" symbol before the variable name.
 	var playerAge int = 22
 	var playerAgePointer *int = &playerAge
 	fmt.Println(playerAgePointer)  // This will print the memory address of the playerAge variable.
 	fmt.Println(*playerAgePointer) // This will print the value of the playerAge variable.
+
+	// ########### Arrays and Slicing ###########
+
+	// ########### Arrays ###########
+	var myFriends = [2]string{"Aneeket", "Nancy"}
+	// This is how, we actually define the arrays in GO. Here "[2]" is the array size
+	// String is the array data type, and we use "{}" for values.
+	// If we already declared the array data type then, we can't store any other data type values in it.
+	fmt.Println(myFriends)
+
+	// ########### Slicing ###########
+	// Slice is an abstraction of an Array
+	// they are more flexible and powerful, also index based and have a size, but is resized when needed.
+	// Basically, In slice we don't need to declare the array size.
+
+	// ########### Loops ###########
+	// Loop statement allows us to execute code multiple times, in a loop, Loops are simplified in GO.
+	// We only have the "For loop" in GO, and can use it for any case
+
+	// for {
+	// 	fmt.Println("My name is Ashish")
+	// }
+	// The above for loop is an infinite loop and it keeps executing until we've not exit it.
 
 }
